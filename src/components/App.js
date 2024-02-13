fe67d66b0cc3eeacd6bf0db82747a94d42e37da8
mk <krasickamagdalena@gmail.com>
2024-02-13 09:33:38 +0000

    implemented timer functionality

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import FinishScreen from "./FinishScreen";
 import Timer from "./Timer";
 import Footer from "./Footer";
 
+const SECS_PER_QUESTION = 30;
 const initialState = {
   questions: [],
   // loading, error, ready, active, finished
@@ -19,6 +20,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -28,7 +30,11 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
     case "start":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
     case "newAnswer":
       const question = state.questions.at(state.index);
       return {
@@ -59,14 +65,22 @@ function reducer(state, action) {
         questions: state.questions,
         status: "ready",
       };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
     default:
       throw new Error("Unknown action type");
   }
 }
 
 function App() {
-  const [{ questions, status, index, answer, points, highscore }, dispatch] =
-    useReducer(reducer, initialState);
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPoints = questions.reduce(
@@ -111,7 +125,7 @@ function App() {
               answer={answer}
             />
             <Footer>
-              <Timer />
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
               <NextButton
                 dispatch={dispatch}
                 answer={answer}
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,30 @@
 import { useEffect } from "react";
 
-function Timer() {
+function Timer({ dispatch, secondsRemaining }) {
+  const minutes = Math.floor(secondsRemaining / 60);
+  console.log(minutes);
+  const seconds = secondsRemaining % 60;
+  console.log(seconds);
   // effects runs twice in the development mode
-  useEffect(function () {
-    setInterval(function () {
-      console.log("tick");
-    }, 1000);
-  }, []);
-  return <div className="timer">05:00</div>;
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+      // clean up function
+      return function () {
+        clearInterval(id);
+      };
+    },
+    [dispatch]
+  );
+  return (
+    <div className="timer">
+      {minutes < 10 && "0"}
+      {minutes} : {seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
 }
 
 export default Timer;
